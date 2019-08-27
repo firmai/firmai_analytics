016ef77ff1d678d9d4a0038122b9c6d5634b7397
marsben <54586433+marsben@users.noreply.github.com>
2019-08-27 19:17:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -361,3 +361,92 @@ $(document).ready(function(){
 
 
  });
+
+
+/* TextScramble Animation*/
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise(resolve => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += char;
+      } else {
+        output += from;
+      }
+    }
+    if (output.indexOf(' ') >=0) {
+      this.el.innerHTML = output;
+    } else {
+      var i = Math.floor(output.length/2)
+      var left = output.slice(0, i)
+      var right = output.slice(i, output.length)
+      this.el.innerHTML = left + '\n' + right
+    }
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+
+
+const phrases = [
+  "Artificial Intelligence",
+  "Machine Learning",
+  "Data Analytics"
+];
+
+const el = document.querySelector(".site-description");
+const fx = new TextScramble(el);
+
+let counter = 0;
+if (900 > 768) {
+
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800);
+    });
+    counter = (counter + 1) % phrases.length;
+  };
+
+  next();
+} else {
+  el.innerHTML = "AI and\nMachine Learning"
+}
